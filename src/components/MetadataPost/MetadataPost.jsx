2207efa05746ba5a3cbfb36610c00b760ddbda7b
Alex Visan <alexandruvisan1908@gmail.com>
2022-10-20 17:23:17 +0300

    css change

--- src/components/MetadataPost/MetadataPost.jsx
+++ src/components/MetadataPost/MetadataPost.jsx
@@ -4,23 +4,26 @@ import { FcClock, FcCalendar } from 'react-icons/fc';
 
 const MetadataPost = ({ author, date, readingTime }) => {
   return (
-    <div className="text-sm md:text-base flex justify-between">
-      <div>
-        {date && author && (
-          <div className="flex">
-            <time className="flex items-center" pubdate="pubdate" dateTime={date}>
-              <FcCalendar className="text-xl" />
-              &nbsp;{formatDate(date)} <TbMinusVertical />
-            </time>
-            <span>By {author.name}</span>
-          </div>
-        )}
+    <div className="pt-3 prose-hr:mt-4 prose-hr:mb-4 md:prose-hr:mt-4 md:prose-hr:mb-4">
+      <div className="text-sm md:text-base flex justify-between">
+        <div>
+          {date && author && (
+            <div className="flex">
+              <time className="flex items-center" pubdate="pubdate" dateTime={date}>
+                <FcCalendar className="text-xl" />
+                &nbsp;{formatDate(date)} <TbMinusVertical />
+              </time>
+              <span>By {author.name}</span>
+            </div>
+          )}
+        </div>
+        <div className="flex items-center">
+          <FcClock className="text-xl" />
+          &nbsp;
+          {readingTime} min read
+        </div>
       </div>
-      <div className="flex items-center">
-        <FcClock className="text-xl" />
-        &nbsp;
-        {readingTime} min read
-      </div>
+      <hr />
     </div>
   );
 };
