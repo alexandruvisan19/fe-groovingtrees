981a4e7752441dac7b41a121444895ada1651bf7
Alex Visan <alexandruvisan1908@gmail.com>
2022-11-07 16:42:18 +0200

    add hover on links

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,5 +1,9 @@
 const Content = ({ children }) => {
-  return <div className="prose-lg prose-p:leading-7">{children}</div>;
+  return (
+    <div className="prose-lg prose-p:leading-7 prose-blue hover:prose-a:text-autumn-500 prose-a:transition prose-a:ease-in-out prose-a:delay-250 prose-a:duration-300">
+      {children}
+    </div>
+  );
 };
 
 export default Content;
